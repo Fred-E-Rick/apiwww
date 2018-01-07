9e45743d47ec2fc34be5653bca8880429ba30492
Fred-E-Rick <fred.tyce@gmail.com>
2018-01-07 18:53:23 +0000

    updated scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
-document.querySelector('html').onclick = function() {
-    alert('Ouch!');
+var Button = document.querySelector('button');
+var Heading = document.querySelector('h1');
+
+function setHeading() {
+    var newHeading = prompt('Please choose a new heading');
+    localStorage.setItem('myHeading', newHeading);
+    Heading.textContent = myHeading;
 }
+
+if(localStorage.getItem('myHeading')) { 
+    Heading.textContent = myHeading;
+};
+
+Button.onclick = function() {
+    setHeading();
+};
