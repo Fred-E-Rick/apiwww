a2c716b654d49aad79fadf4bc39ceb25c5af5d87
Fred-E-Rick <fred.tyce@gmail.com>
2018-01-14 01:34:53 +0000

    gfd

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 70) {
-        window.location.replace("http://fred-e-rick.tk/f");
+        window.location.href = "http://fred-e-rick.tk/f";
     } 
     if(event.keyCode != 70) {
-        window.location.replace("https://fred-e-rick.tk/not-f");
+        window.location.href = "https://fred-e-rick.tk/not-f";
     }
     
 });
