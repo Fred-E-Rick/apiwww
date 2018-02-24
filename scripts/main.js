68a23de6b10f9a4e5ed3528eef92e5324edc8388
Fred-E-Rick <30805402+Fred-E-Rick@users.noreply.github.com>
2018-02-24 19:01:19 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 70) {
-        window.location.href = "http://fred-e-rick.tk/f";
+        window.location.href = "../f";
     } 
     if(event.keyCode != 70) {
-        window.location.href = "https://fred-e-rick.tk/not-f";
+        window.location.href = "../not-f";
     }
     
 });
